4e6407867d86ba7718703c0f70359ec78cc9e32e
Valeria Serci <balerty84@gmail.com>
2024-07-17 14:43:02 +0100

    add a message in case no meetings for that day

---
+++ src/main/scala/Meeting.scala
@@ -0,0 +1,10 @@
+class Meeting (val name: String, val day: String, val time: String)
+
+object Main extends App {
+  val m1 = new Meeting("Retro", "Friday", "5pm")
+  val m2 = new Meeting("Yoga", "Tuesday", "10am")
+  val m3 = new Meeting("Team Meeting", "Tuesday", "3pm")
+  val agenda = new Agenda(List(m1, m2, m3))
+  agenda.printDaySchedule("Monday")
+  agenda.printDaySchedule("Tuesday")
+}
