581cf15f0303146ea85da106b436939a0859d54d
Valeria Serci <balerty84@gmail.com>
2024-07-17 21:35:02 +0100

    working version of displaying message when no events and am and pm displayed separately

--- src/main/scala/Agenda.scala
+++ src/main/scala/Agenda.scala
@@ -5,10 +5,29 @@ class Agenda (val meetings: List[Meeting]) {
     if (meetingsForTheDay.isEmpty) {
       println(s"There are no meetings on $day")
     } else {
-        println(s"$day:")
-        for (meeting <- meetingsForTheDay) println(s"  ${meeting.time}: ${meeting.name}")
+      val morning = meetingsForTheDay.filter(x => !isAfternoon(x.time))
+      if (morning.isEmpty) {
+        println(s"There are no meetings on $day morning")
+      } else {
+        println(s"$day morning: ")
+        for (meeting <- morning) println(s"  ${meeting.time}: ${meeting.name}")
+      }
+      val afternoon = meetingsForTheDay.filter(x => isAfternoon(x.time))
+      if (afternoon.isEmpty) {
+        println(s"There are no meetings on $day afternoon")
+      } else {
+        println(s"$day afternoon: ")
+        for (meeting <- afternoon) println(s"  ${meeting.time}: ${meeting.name}")
+      }
+
+
+
+
     }
   }
 
+  private def isAfternoon(time: String): Boolean = {
+    time contains "pm"
+  }
 }
 
